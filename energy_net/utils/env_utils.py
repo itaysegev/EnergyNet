100ab2be516aae027dea8a00b32832be3bc12c5c
Itay Segev <itaysegev11@gmail.com>
2024-06-27 18:25:49 +0300

    Update according to the uml

--- energy_net/utils/env_utils.py
+++ energy_net/utils/env_utils.py
@@ -7,12 +7,12 @@ import time
 from ..defs import Bounds
 from ..config import DEFAULT_EFFICIENCY, DEFAULT_LIFETIME_CONSTANT
 from ..entities.params import StorageParams, ProductionParams, ConsumptionParams
-from ..network_entity import NetworkEntity
-from ..entities.local_storage import Battery
+from ..entities.network_entity import NetworkEntity
+from ..devices.storage_devices.local_storage import Battery
 from ..dynamics.storage_dynamics import BatteryDynamics
-from ..entities.local_producer import PrivateProducer
+from ..devices.production_devices.local_producer import PrivateProducer
 from ..dynamics.production_dynamics import PVDynamics
-from ..entities.consumer_device import ConsumerDevice
+from ..devices.consumer_devices.consumer_device import ConsumerDevice
 from ..dynamics.consumption_dynamics import PCSUnitConsumptionDynamics
 from ..entities.pcsunit import PCSUnit
 from ..model.reward import RewardFunction
@@ -42,29 +42,6 @@ def bounds_to_gym_box(bounds: Bounds) -> Box:
         shape=bounds['shape'],
         dtype=bounds['dtype']
     )
-
-
-def default_pcsunit():
-    # initialize consumer devices
-        consumption_params_arr=[]
-        consumption_params = ConsumptionParams(name='pcsunit_consumption', energy_dynamics=PCSUnitConsumptionDynamics(), lifetime_constant=DEFAULT_LIFETIME_CONSTANT)
-        consumption_params_arr.append(consumption_params)
-        consumption_params_dict = {'pcsunit_consumption': consumption_params}
-
-        # initialize storage devices
-        storage_params_arr=[]
-        storage_params = StorageParams(name = 'test_battery', energy_capacity = 100, power_capacity = 200,inital_charge = 50, charging_efficiency = 1,discharging_efficiency = 1, lifetime_constant = 15, energy_dynamics = BatteryDynamics())
-        storage_params_arr.append(storage_params)
-        storage_params_dict = {'test_battery': storage_params}
-
-        # initialize production devices
-        production_params_arr=[]
-        production_params = ProductionParams(name='test_pv', max_production=100, efficiency=0.9, energy_dynamics=PVDynamics())
-        production_params_arr.append(production_params)
-        production_params_dict = {'test_pv': production_params}
-
-        # initilaize pcsunit
-        return PCSUnit(name="test_pcsunit", consumption_params_dict=consumption_params_dict, storage_params_dict=storage_params_dict, production_params_dict=production_params_dict, agg_func= None)
 
 
 def default_network_entities() -> List[NetworkEntity]:
