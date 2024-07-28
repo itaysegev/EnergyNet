c81d0f7e5523c2caea6d5d7f31b75153274d8da8
Itay Segev <itaysegev11@gmail.com>
2024-07-28 14:42:56 +0300

    Add rl zoo support

--- tests/test_env_demo.py
+++ tests/test_env_demo.py
@@ -7,15 +7,28 @@ from energy_net.network import Network
 from energy_net.stratigic_entity import StrategicEntity
 
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3 import SAC
+from stable_baselines3 import PPO
+import time
+import os
 
 def simulation_reward_function(state, action, new_state):
-    price = state.get_price()
     grid_electricity = max(action.item() - state.get_consumption() + state.get_production(), 0)
+    price = grid_electricity
     return -1 * price * grid_electricity
 
 
 def main():
+    
+    models_dir = f"models/{int(time.time())}/"
+    logdir = f"logs/{int(time.time())}/"
+    
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+	    
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
     # simulate the environment
     strategic_entities = [StrategicEntity(name="pcs_agent", network_entity=default_pcsunit(), reward_function=simulation_reward_function)]
     network =  Network(name="test_network", strategic_entities=strategic_entities)
@@ -28,19 +41,16 @@ def main():
         print('Passed test!! EnergyNetEnv is compatible with SB3 when using the StableBaselines3Wrapper.')
     finally:
         pass
-    model = SAC('MlpPolicy', env, verbose=2, learning_starts=env.time_steps, seed=24)
+    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
     # model.learn(total_timesteps=env.time_steps*3, log_interval=1)
     obs, _ = env.reset()
-    print(obs, 'init_obs')
+    TIMESTEPS = 10000
+    iters = 0
     while True:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, rewards, terms, truncs, infos = env.step(action)
-        print(rewards, 'rewards')
-        print(obs, 'obs')
-        if terms or truncs:
-            break
+        iters += 1
+        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
+        model.save(f"{models_dir}/{TIMESTEPS*iters}")
         
-    env.close()
     
 
 if __name__ == '__main__':
